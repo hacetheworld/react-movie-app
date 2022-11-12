7a6fd02f73d3bea0f2344ce0103049d06fce2348
Ajay <majay1638@gmail.com>
2022-11-12 20:16:04 +0530

    initial commit

---
+++ frontend/movie-app/src/App.js
@@ -0,0 +1,15 @@
+import { Component } from "react";
+import HomePage from "./homepage";
+import "./App.css"
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() {
+    return (<HomePage/>)
+  }
+}
+
+export default App;
